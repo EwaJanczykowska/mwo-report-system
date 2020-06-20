58f997002dbfa971b6a6d0acbd2d1630ac1a0631
Iza <isabela.ann@gmail.com>
2020-06-20 13:18:28 +0200

    Izabella: Model for input data

---
+++ src/main/java/pl/edu/agh/mwo/reporter/model/Person.java
@@ -0,0 +1,31 @@
+package pl.edu.agh.mwo.reporter.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Person {
+    private final String name;
+    private final List<Task> tasks;
+
+    public Person(String name) {
+        this.name = name;
+        this.tasks = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void addTask(Task task) {
+        this.tasks.add(task);
+    }
+
+    public void addTasks(Collection<Task> tasks) {
+        this.tasks.addAll(tasks);
+    }
+}
