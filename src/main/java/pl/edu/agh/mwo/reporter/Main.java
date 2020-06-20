ec7cb2b7528807124a5a2bd8babd7dc938580c50
kniadziu <36226135+kniadziu@users.noreply.github.com>
2020-06-20 13:51:04 +0200

    dodoano wczytywanie plikow sciezek do Excela

--- src/main/java/pl/edu/agh/mwo/reporter/Main.java
+++ src/main/java/pl/edu/agh/mwo/reporter/Main.java
@@ -1,8 +1,14 @@
 package pl.edu.agh.mwo.reporter;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        ReaderExcelFiles f= new ReaderExcelFiles();
+         f.getAllFiles("C:\\");
+   
 
     }
 
