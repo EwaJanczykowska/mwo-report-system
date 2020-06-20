36bf22f484f622bf27fe3b6d7e0ff5d7bfa30061
Agnieszka Wegrzyn <wegrzynagnieszka94@gmail.com>
2020-06-20 14:13:51 +0200

    Printing to console implemented

--- src/main/java/pl/edu/agh/mwo/reporter/reports/Report1.java
+++ src/main/java/pl/edu/agh/mwo/reporter/reports/Report1.java
@@ -2,6 +2,7 @@ package pl.edu.agh.mwo.reporter.reports;
 
 import pl.edu.agh.mwo.reporter.model.Company;
 import pl.edu.agh.mwo.reporter.model.Person;
+import pl.edu.agh.mwo.reporter.model.Task;
 
 import java.util.List;
 
@@ -11,6 +12,17 @@ public class Report1 implements ReportGenerator {
 
     public void printToConsole(Company company) {
         this.personList = company.getPersons();
+
+        System.out.printf("%-40s %-15s\n", "Imie nazwisko", "Liczba godzin");
+
+        for (Person person : personList) {
+            int hours = 0;
+            for (Task task : person.getTasks()) {
+                hours += task.getHours();
+            }
+            System.out.printf("%-40s %-15d\n", person.getName(), hours);
+        }
+
     }
 
     public void printToExcel(Company company) {
