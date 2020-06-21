99a1a4287b0fde22a7a9959b727d728306b092d4
Ewa Janczykowska <janczykowska@student.agh.edu.pl>
2020-06-21 10:04:09 +0200

    Loading data from multiple excel files

--- src/test/java/pl/edu/agh/mwo/reporter/loader/DataLoaderTest.java
+++ src/test/java/pl/edu/agh/mwo/reporter/loader/DataLoaderTest.java
@@ -5,7 +5,9 @@ import org.junit.Test;
 import pl.edu.agh.mwo.reporter.model.Company;
 import pl.edu.agh.mwo.reporter.model.Person;
 
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 public class DataLoaderTest {
@@ -14,16 +16,22 @@ public class DataLoaderTest {
     public void read_jan_kowalski_data() throws Exception {
         DataLoader dataLoader = new DataLoader();
 
-        Company company = dataLoader.loadData(Paths.get("resources/2012/01/Kowalski_Jan.xls"));
+        List<Path> paths = Arrays.asList(Paths.get("resources/2012/01/Kowalski_Jan.xls"), Paths.get("resources/2012/01/Nowak_Piotr.xls"));
+
+        Company company = dataLoader.loadData(paths);
 
         Assert.assertNotNull(company);
         List<Person> persons = company.getPersons();
 
-        Assert.assertEquals(1, persons.size());
-        Person person = persons.get(0);
+        Assert.assertEquals(2, persons.size());
 
-        Assert.assertEquals("Kowalski Jan", person.getName());
-        Assert.assertEquals(11, person.getTasks().size());
+        Person person0 = persons.get(0);
+        Assert.assertEquals("Kowalski Jan", person0.getName());
+        Assert.assertEquals(11, person0.getTasks().size());
+
+        Person person1 = persons.get(1);
+        Assert.assertEquals("Nowak Piotr", person1.getName());
+        Assert.assertEquals(10, person1.getTasks().size());
 
         System.out.println(company);
     }
